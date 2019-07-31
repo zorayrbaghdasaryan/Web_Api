6ae17903f0718f0e31b7c788282bdfdd2547368f
Zorayr <zorayr.baghdasaryan@esterox.am>
2019-07-31 18:59:05 +0400

    add

--- IShop/Controllers/CategoriesController.cs
+++ IShop/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using IShop.BusinessLogic.Services;
+using IShop.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +13,36 @@ namespace IShop.Controllers
     {
         private ICategoryService _categoryService = new CategoryService();
 
+        [HttpGet]
         public IHttpActionResult GetAll()
         {
             return Ok(_categoryService.GetAll());
         }
+
+        [HttpGet]
         public IHttpActionResult Get(int id)
         {
             return Ok(_categoryService.Get(id));
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody]Category category)
+        {
+            _categoryService.Add(category);
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult Update([FromBody]Category category)
+        {
+            _categoryService.Update(category);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delet(int id)
+        {
+            _categoryService.Delete(id);
+            return Ok();
         }
     }
 }
